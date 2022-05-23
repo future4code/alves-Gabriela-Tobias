76756fd1e95e3d6e0b548871b7332f4a3cbc5b82
Gabriela Tobias <gabih_tobias@hotmail.com>
2022-05-23 17:15:08 -0300

    exercicio 6

--- modulo1/lista-revisao-js/exercicios.js
+++ modulo1/lista-revisao-js/exercicios.js
@@ -15,23 +15,30 @@ function retornaArrayInvertido(array) {
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-  return array.sort((a,b)=>a-b)
+  return array.sort((a, b) => a - b)
 }
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-return array.filter((value)=>{
-  if (value%2===0)
-  return value })
- }
+  return array.filter((value) => {
+    if (value % 2 === 0)
+      return value
+  })
+}
 
 // EXERCÍCIO 05
-function retornaNumerosParesElevadosADois(array) { 
-  return array.filter((value)=> {if (value%2===0) return value}).map ((value)=>value**2) 
+function retornaNumerosParesElevadosADois(array) {
+  return array.filter((value) => { if (value % 2 === 0) return value }).map((value) => value ** 2)
 }
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
+  let numeroMaior = array[0]
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > numeroMaior) {
+      numeroMaior = array[i]
+    }
+  }
+  return numeroMaior
 }
 
 // EXERCÍCIO 07
@@ -41,7 +48,7 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+
 }
 
 // EXERCÍCIO 09
@@ -51,27 +58,27 @@ function classificaTriangulo(ladoA, ladoB, ladoC) {
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+
 }
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+
 }
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+
 }
 
 // EXERCÍCIO 14
@@ -81,10 +88,10 @@ function retornaContasComSaldoAtualizado(contas) {
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-  
+
 }
 
 // EXERCÍCIO 15B
 function retornaArrayOrdenadoPorData(consultas) {
-   
- }
+
+}
