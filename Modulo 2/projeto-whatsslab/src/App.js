e7892dd4249ceb4dca41767e553d9baff44534ac
Gabriela Tobias <gabih_tobias@hotmail.com>
2022-06-12 21:11:48 -0300

    projeto-whatslabfinal

---
+++ Modulo 2/projeto-whatsslab/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CaixaUsuario from './components/CaixaUsuario'
+import styled from 'styled-components'
+
+function App() {
+
+  return (
+    <div>
+      <CaixaUsuario/>
+    </div>
+  );
+}
+
+export default App;
