66c279cdc5cf450190b560beac014dc11986130a
Gabriela Tobias <gabih_tobias@hotmail.com>
2022-06-26 22:47:39 -0300

    projetolabefy

--- Modulo 2/projeto-labefy/src/App.js
+++ Modulo 2/projeto-labefy/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import CriaPlaylist from './Components/CriaPlaylist';
+import Tela from './Components/Tela';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Fundo = styled.div`
+  background-color: #221F1F;
+  display: flex;
+  align-items: center;
+  width: 100%;
+`
+const Conteudo = styled.div`
+  width: 100%;
+  height: 80vh;
+  margin: 50px auto;
+  text-align: center;
+  background-color: #221F1F;
+
+
+  `
+export default class App extends Component {
+  render() {
+    return (
+      <Fundo>
+        <Conteudo>
+          <CriaPlaylist />
+          <Tela />
+        </Conteudo>
+      </Fundo>
+    )
+  }
 }
-
-export default App;
