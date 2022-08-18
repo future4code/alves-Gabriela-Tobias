de070e253f10033c98e39d5b6ddd8b67fd6ee4d2
Gabriela Tobias <gabih_tobias@hotmail.com>
2022-08-17 21:29:15 -0300

    fluxo

---
+++ Modulo 4/fluxo-de-dados/src/data.ts
@@ -0,0 +1,17 @@
+export const products = [
+    {
+        id: 1,
+        name: 'Arroz',
+        price: 15,
+    },
+    {
+        id: 2,
+        name: 'Feijão',
+        price: 10,
+    },
+    {
+        id: 3,
+        name: 'Macarrão',
+        price: 3,
+    }
+]
