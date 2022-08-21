a361c3ffe56fec48dd1d1f4b9a73a6aa781c905b
Gabriela Tobias <gabih_tobias@hotmail.com>
2022-08-21 18:39:46 -0300

    banco

---
+++ Modulo 4/sistema-bancario/src/accounts.ts
@@ -0,0 +1,27 @@
+import { Account } from "./types";
+
+export const accounts: Account[] = []
+
+// {
+    //     name: "Murilo",
+    //     cpf: "000.000.666-33",
+    //     dateOfBirth: "10-10-2010",
+    //     balance: 666,
+    //     statement: []
+    // },
+    // {
+    //     name: "Tchuchuca",
+    //     cpf: "666.666.666-66",
+    //     dateOfBirth: 06-06-1966,
+    //     balance: 600,
+    //     statement: []
+
+    // },
+    // {
+    //     name: "Tchuca",
+    //     cpf: "898.666.999-66",
+    //     dateOfBirth: 06-06-2006,
+    //     balance: 100,
+    //     statement: []
+    // }
+// ]
